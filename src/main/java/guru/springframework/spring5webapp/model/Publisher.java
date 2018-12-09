0aac7e8b8fb8698d9c0d4076d7bd0be023c823d2
thorofshun <dingshun.julian@gmail.com>
2018-12-09 16:14:29 +0800

    Lecture23

--- src/main/java/guru/springframework/spring5webapp/model/Publisher.java
+++ src/main/java/guru/springframework/spring5webapp/model/Publisher.java
@@ -17,6 +17,9 @@ public class Publisher {
     private String name;
     private String address;
 
+    public Publisher() {
+    }
+
     public Publisher(String name, String address) {
         this.name = name;
         this.address = address;
